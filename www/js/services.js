676622c7cfde3c5b66ff952691d0603c144ea12e
aleeblanco <alee_antonio@outlook.com>
2015-09-12 01:26:01 +0000

    Modificacion de imagenes

--- www/js/services.js
+++ www/js/services.js
@@ -7,28 +7,28 @@ angular.module('starter.services', [])
   var chats = [{
     id: 0,
     name: 'Janeth Valadez',
-    lastText: 'You on your way?',
+    lastText: 'Llegare tarde peladuki',
     face: 'https://scontent-lax3-1.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/12006208_10204508295536702_3120195145328511434_n.jpg?oh=b84ba7e93b306fd895acf7ec0986ec27&oe=56A99DD5'
   }, {
     id: 1,
     name: 'Arlenne Mota',
-    lastText: 'Hey, it\'s me',
+    lastText: 'Hello',
     face: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xap1/v/t1.0-1/1907382_842463272477076_5419426880409256730_n.jpg?oh=43b5e3a023b99783e6d1a7d1acae1f87&oe=5662BA81&__gda__=1448964387_bc8a8bed03491f152148bba1d89ea2a1'
   }, {
     id: 2,
     name: 'Arely Valdes',
-    lastText: 'I should buy a boat',
+    lastText: 'Hola',
     face: 'https://scontent-lax3-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/11390310_10202942223316124_6761803190300081432_n.jpg?oh=10ee16f8240f0d80d8d2b953d65e148a&oe=5664E99D'
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+    name: 'Isela Briseño',
+    lastText: 'Donde andan mujeres',
+    face: 'https://scontent-dfw1-1.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/1898253_10206717763416914_1694988025160312490_n.jpg?oh=2b3025dc32d8878f5d8c41c8e449a8b1&oe=56A23A67'
   }, {
     id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+    name: 'Karlita Santos',
+    lastText: '¿Ya vienes?',
+    face: 'https://scontent-dfw1-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/11987181_928133407253125_7714961305001722102_n.jpg?oh=9e70fe7701f181ba79a96bc9c7ffeb2a&oe=56AB0ECA'
   }];
 
   return {
